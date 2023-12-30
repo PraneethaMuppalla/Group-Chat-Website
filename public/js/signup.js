3739fcc5f5fbde606a47d1415dcf3d7df1ef8d55
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2023-12-30 11:34:04 +0530

    Signup frontend

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,6 +4,7 @@ const axiosInstance = axios.create({
 
 const signUpFormEl = document.getElementById("form");
 const nameEl = document.getElementById("name");
+const phoneNumEl = document.getElementById("phoneNum");
 const emailEl = document.getElementById("email");
 const passwordEl = document.getElementById("password");
 
@@ -13,21 +14,24 @@ async function submitSignUpForm(e) {
     const newUser = {
       name: nameEl.value,
       email: emailEl.value,
+      phoneNum: phoneNumEl.value,
       password: passwordEl.value,
     };
     const response = await axiosInstance.post("/sign-up", newUser);
     nameEl.value = "";
     emailEl.value = "";
     passwordEl.value = "";
+    phoneNumEl.value = "";
     window.location.href = `/login.html`;
   } catch (err) {
     if (err.response && err.response.status === 409) {
-      window.location.href = `./login.html?error=1`;
+      window.location.href = `./login.html`;
       nameEl.value = "";
       emailEl.value = "";
       passwordEl.value = "";
+      phoneNumEl.value = "";
     } else {
-      errorToast("Some error occured. Please try again.");
+      alert("Some error occured. Please try again.");
     }
   }
 }
