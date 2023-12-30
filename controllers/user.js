a6efb440b240a47888fd65049d21d70e7a9c5832
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2023-12-30 16:36:52 +0530

    Login frontend

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const { Op } = require("sequelize");
-
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const { use } = require("../routes/user");
 
@@ -10,6 +10,10 @@ function isStringInValid(string) {
   } else {
     return false;
   }
+}
+
+function generateToken(userId) {
+  return jwt.sign(userId, process.env.TOKEN_SECRET);
 }
 
 exports.signUpUser = async (req, res, next) => {
@@ -46,6 +50,40 @@ exports.signUpUser = async (req, res, next) => {
       return res
         .status(201)
         .json({ success: true, msg: "User registration successful." });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, msg: err });
+  }
+};
+
+exports.loginUser = async (req, res, next) => {
+  try {
+    const { phoneNum, password } = req.body;
+    if (isStringInValid(phoneNum) || isStringInValid(password)) {
+      return res.status(400).json({ success: false, err: "Bad parameters" });
+    }
+    const users = await User.findAll({
+      where: { phoneNum },
+    });
+    const user = users[0];
+    if (!user) {
+      //user not registered
+      return res.status(404).json({ success: false, msg: "User not found" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      //unauthorized
+      return res
+        .status(401)
+        .json({ success: false, msg: "Phone Number or password is incorrect" });
+    } else {
+      const token = generateToken(user.id);
+      return res.status(200).json({
+        success: true,
+        msg: "Successful Login",
+        token,
+      });
     }
   } catch (err) {
     console.error(err);
